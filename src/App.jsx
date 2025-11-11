f2a51a7df9f0bbca52aa52d2aa0c43885f998808
vivek <vivekcloud123@gmail.com>
2025-11-11 22:34:27 +0530

    pushd

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
     try {
       setLoading(true);
       setReview(""); 
-      const response = await axios.post("https://backend-resume-vercel-dzpx.vercel.app/", {
+      const response = await axios.post("https://backend-resume-vercel-dzpx.vercel.app", {
         code,
       });
       setReview(response.data);
